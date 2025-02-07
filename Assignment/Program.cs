02c93943934a0d5fdf8fea856b8e5d6b3ae0a77a
Imagdy00 <125517448+Imagdy00@users.noreply.github.com>
2025-02-07 18:41:37 +0200

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,143 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        #region Count numbers greater than X
+        //static void CountGreaterNumbers(int[] arr, int[] queries)
+        //{
+        //    foreach (int x in queries)
+        //    {
+        //        Console.WriteLine(arr.Count(n => n > x));
+        //    }
+        //}
+        #endregion
+
+
+        #region Check if an array is a palindrome
+        //static string IsPalindrome(int[] arr)
+        //{
+        //    return arr.SequenceEqual(arr.Reverse()) ? "YES" : "NO";
+        //}
+
+        #endregion
+
+
+        #region Reverse a queue using a stack
+        //static Queue<int> ReverseQueue(Queue<int> queue)
+        //{
+        //    Stack<int> stack = new Stack<int>();
+        //    while (queue.Count > 0)
+        //        stack.Push(queue.Dequeue());
+        //    while (stack.Count > 0)
+        //        queue.Enqueue(stack.Pop());
+        //    return queue;
+        //}
+        #endregion
+
+
+
+        #region Check balanced parentheses
+        //static string IsBalanced(string s)
+        //{
+        //    Stack<char> stack = new Stack<char>();
+        //    foreach (char c in s)
+        //    {
+        //        if ("({[".Contains(c)) stack.Push(c);
+        //        else if (stack.Count == 0 || "(){}[]".IndexOf(stack.Pop() + "" + c) == -1)
+        //            return "Unbalanced";
+        //    }
+        //    return stack.Count == 0 ? "Balanced" : "Unbalanced";
+        //}
+        #endregion
+
+
+        #region Remove duplicates from an array
+        //static int[] RemoveDuplicates(int[] arr)
+        //{
+        //    return arr.Distinct().ToArray();
+        //}
+        #endregion
+
+
+        #region Remove all odd numbers
+
+        //static List<int> RemoveOddNumbers(List<int> list)
+        //{
+        //    return list.Where(n => n % 2 == 0).ToList();
+        //}
+        #endregion
+
+
+        #region Generic queue implementation
+        //class GenericQueue<T>
+        //{
+        //    private Queue<T> queue = new Queue<T>();
+        //    public void Enqueue(T item) => queue.Enqueue(item);
+        //    public T Dequeue() => queue.Dequeue();
+        //}
+        #endregion
+
+
+
+        #region Search for an integer in a stack
+        //static void SearchStack(Stack<int> stack, int target)
+        //{
+        //    int count = 0;
+        //    foreach (int num in stack)
+        //    {
+        //        count++;
+        //        if (num == target)
+        //        {
+        //            Console.WriteLine($"Target was found successfully and the count = {count}");
+        //            return;
+        //        }
+        //    }
+        //    Console.WriteLine("Target was not found");
+        //}
+        #endregion
+
+
+        #region Find intersection of two arrays
+        //static int[] FindIntersection(int[] arr1, int[] arr2)
+        //{
+        //    return arr1.GroupBy(n => n)
+        //               .SelectMany(g => Enumerable.Repeat(g.Key, Math.Min(g.Count(), arr2.Count(n => n == g.Key))))
+        //               .ToArray();
+        //}
+        #endregion
+
+
+
+        #region Find contiguous sublist with target sum
+        //static List<int> FindSublistWithSum(List<int> list, int target)
+        //{
+        //    for (int i = 0; i < list.Count; i++)
+        //    {
+        //        int sum = 0;
+        //        for (int j = i; j < list.Count; j++)
+        //        {
+        //            sum += list[j];
+        //            if (sum == target)
+        //                return list.GetRange(i, j - i + 1);
+        //        }
+        //    }
+        //    return new List<int>();
+        //}
+        #endregion
+
+
+        #region Reverse first K elements of a queue
+        //static Queue<int> ReverseFirstK(Queue<int> queue, int k)
+        //{
+        //    Stack<int> stack = new Stack<int>();
+        //    for (int i = 0; i < k; i++)
+        //        stack.Push(queue.Dequeue());
+        //    while (stack.Count > 0)
+        //        queue.Enqueue(stack.Pop());
+        //    for (int i = 0; i < queue.Count - k; i++)
+        //        queue.Enqueue(queue.Dequeue());
+        //    return queue;
+        //}
+        #endregion
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region video 1
+            //            using System;
+            //            using System.Collections;
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                ArrayList myList = new ArrayList();
+            //                myList.Add(1);
+            //                myList.Add("Hello");
+            //                myList.Add(3.14);
+            //                foreach (var item in myList)
+            //                {
+            //                    Console.WriteLine(item);
+            //                }
+            //            }
+            //        }
+            #endregion
+
+
+            #region video 2
+            //            using System;
+            //            using System.Collections;
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                ArrayList list = new ArrayList();
+            //                list.Add(1);
+            //                list.Add("Two");
+            //                list.Add(3.0);
+            //                foreach (var item in list)
+            //                {
+            //                    Console.WriteLine(item);
+            //                }
+            //            }
+            //        }
+
+            #endregion
+
+
+
+            #region video 3
+            //            using System;
+            //            using System.Collections;
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                ArrayList list = new ArrayList();
+            //                list.Add(1);
+            //                list.Add("Two"); // No compile-time error
+            //                                 // int num = (int)list[1]; // Runtime error
+            //            }
+            //        }
+            #endregion
+
+
+
+            #region video 4
+            //            using System;
+            //            using System.Collections.Generic;
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                List<int> numbers = new List<int>();
+            //                numbers.Add(1);
+            //                numbers.Add(2);
+            //                numbers.Add(3);
+            //                foreach (int number in numbers)
+            //                {
+            //                    Console.WriteLine(number);
+            //                }
+            //            }
+            //        }
+            #endregion
+
+
+
+            #region video 5
+            //            using System;
+            //            using System.Collections.Generic;
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                LinkedList<string> list = new LinkedList<string>();
+            //                list.AddLast("Apple");
+            //                list.AddLast("Banana");
+            //                list.AddLast("Cherry");
+            //                foreach (string fruit in list)
+            //                {
+            //                    Console.WriteLine(fruit);
+            //                }
+            //            }
+            //        }
+            #endregion
+
+
+
+
+            #region video 6
+            //            using System;
+            //            using System.Collections.Generic;
+
+            //class Program
+            //        {
+            //            static void Main()
+            //            {
+            //                Stack<int> stack = new Stack<int>();
+            //                stack.Push(1);
+            //                stack.Push(2);
+            //                stack.Push(3);
+            //                while (stack.Count > 0)
+            //                {
+            //                    Console.WriteLine(stack.Pop());
+            //                }
+            //            }
+            //        }
+            #endregion
+
+            #region video 7
+//            using System;
+//            using System.Collections.Generic;
+
+//class Program
+//        {
+//            static void Main()
+//            {
+//                Queue<string> queue = new Queue<string>();
+//                queue.Enqueue("First");
+//                queue.Enqueue("Second");
+//                queue.Enqueue("Third");
+//                while (queue.Count > 0)
+//                {
+//                    Console.WriteLine(queue.Dequeue());
+//                }
+//            }
+//        }
+        #endregion
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    }
+}
